ba92a7fc2f4b1798c441d0de0263901fe6c75506
Louis Taylor <kragniz@gmail.com>
2013-12-02 17:52:21 +0000

    Add class skeletons

---
+++ DOCS/drafts/class_diagrams/TourUploader.java
@@ -0,0 +1,12 @@
+
+public class TourUploader {
+	Tour[] tours;
+	
+	/*
+	 * Upload the tours to the server.
+	 * Return true if the upload was successful.
+	 */
+	public boolean upload() {
+		return false;
+	}
+}
