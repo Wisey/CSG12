4bdc684b6d2c68840af2996d0dbecd73dc16322d
Louis Taylor <kragniz@gmail.com>
2013-12-09 10:45:28 +0000

    Move location behaviour into a new activity

--- SRC/Drafts/WalkingTour/src/uk/ac/aber/group12/walkingtour/HomeActivity.java
+++ SRC/Drafts/WalkingTour/src/uk/ac/aber/group12/walkingtour/HomeActivity.java
@@ -1,33 +1,22 @@
 package uk.ac.aber.group12.walkingtour;
 
-import android.location.Criteria;
-import android.location.Location;
-import android.location.LocationListener;
-import android.location.LocationManager;
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
 import android.view.Menu;
 import android.view.View;
-import android.widget.Toast;
 
-public class HomeActivity extends Activity implements LocationListener {
-	private LocationManager locationManager;
-	private String provider;
-	private double latitude = 0;
-	private double longitude = 0;
+public class HomeActivity extends Activity {
+	
+	public void onStartTourCreator(View view) {
+		Intent intent = new Intent(this, TourCreatorActivity.class);
+		startActivity(intent);
+	}
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_home);
-		locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
-
-		Criteria criteria = new Criteria();
-		provider = locationManager.getBestProvider(criteria, false);
-		Location location = locationManager.getLastKnownLocation(provider);
-		if (location != null) {
-			onLocationChanged(location);
-		}
 	}
 
 	@Override
@@ -37,46 +26,5 @@ public class HomeActivity extends Activity implements LocationListener {
 		return true;
 	}
 
-	public void onCoordinateClick(View view) {
-		String Text = "Latitude = " + latitude + " Longitude = " + longitude;
-		Toast.makeText(getApplicationContext(), Text, Toast.LENGTH_SHORT)
-				.show();
-	}
 
-	public void getCoordinates() {
-
-	}
-
-	@Override
-	public void onLocationChanged(Location location) {
-		location.getLatitude();
-		location.getLongitude();
-		System.out.println("location changed");
-		latitude = location.getLatitude();
-		longitude = location.getLongitude();
-	}
-
-	@Override
-	public void onProviderDisabled(String arg0) {
-	}
-
-	@Override
-	public void onProviderEnabled(String arg0) {
-	}
-
-	@Override
-	public void onStatusChanged(String arg0, int arg1, Bundle arg2) {
-	}
-
-	@Override
-	protected void onResume() {
-		super.onResume();
-		locationManager.requestLocationUpdates(provider, 400, 1, this);
-	}
-
-	@Override
-	protected void onPause() {
-		super.onPause();
-		locationManager.removeUpdates(this);
-	}
 }
