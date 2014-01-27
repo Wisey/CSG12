1dd117c0b44584d8cc63539732e05b69f548da67
Louis Taylor <kragniz@gmail.com>
2014-01-27 16:16:28 +0000

    Decode received json

--- SRC/Drafts/Mocks/receive.py
+++ SRC/Drafts/Mocks/receive.py
@@ -1,11 +1,12 @@
 from flask import Flask, request
+import json
 app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
 def hello_world():
     if request.method == 'POST':
-        print request.data
-        return 'success'
+        print json.loads(request.data.strip())
+        
     else:
         return 'POST to this url, please\n'
 
