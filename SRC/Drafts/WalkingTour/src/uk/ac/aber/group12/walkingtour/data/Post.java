62cbce8a75c1e8edef8bbf1dc9e5a94122b0d8f6
Louis Taylor <kragniz@gmail.com>
2014-01-28 12:04:53 +0000

    Use apache HTTPResponse as the POST client

--- SRC/Drafts/WalkingTour/src/uk/ac/aber/group12/walkingtour/data/Post.java
+++ SRC/Drafts/WalkingTour/src/uk/ac/aber/group12/walkingtour/data/Post.java
@@ -1,21 +1,39 @@
 package uk.ac.aber.group12.walkingtour.data;
 
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.ByteArrayEntity;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.params.BasicHttpParams;
+import org.apache.http.params.HttpConnectionParams;
+import org.apache.http.params.HttpParams;
+
 import java.io.IOException;
-import java.net.MalformedURLException;
-import java.net.URL;
-import java.net.URLConnection;
+import java.io.UnsupportedEncodingException;
 
 /**
  * Created by lot15 on 28/01/2014.
  */
+
 public class Post {
-    public static boolean postJSON(String urlString) {
+    public static boolean postJSON(String urlString, String postBody) {
+        int TIMEOUT = 10000;
+        HttpParams httpParams = new BasicHttpParams();
+        HttpConnectionParams.setConnectionTimeout(httpParams, TIMEOUT);
+        HttpConnectionParams.setSoTimeout(httpParams, TIMEOUT);
+        HttpClient client = new DefaultHttpClient(httpParams);
+
+        HttpPost request = new HttpPost(urlString);
         try {
-            URL url = new URL(urlString);
-            URLConnection conn = url.openConnection();
-        } catch (MalformedURLException e) {
+            request.setEntity(new ByteArrayEntity(
+                    postBody.toString().getBytes("UTF8")));
+        } catch (UnsupportedEncodingException e) {
             e.printStackTrace();
             return false;
+        }
+        try {
+            HttpResponse response = client.execute(request);
         } catch (IOException e) {
             e.printStackTrace();
             return false;
