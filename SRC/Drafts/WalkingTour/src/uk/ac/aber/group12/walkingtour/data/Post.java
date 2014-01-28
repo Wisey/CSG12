1e5357a1bec49bec15a2e568566a6e37532fd75d
Louis Taylor <kragniz@gmail.com>
2014-01-28 11:42:31 +0000

    Create a url object

--- SRC/Drafts/WalkingTour/src/uk/ac/aber/group12/walkingtour/data/Post.java
+++ SRC/Drafts/WalkingTour/src/uk/ac/aber/group12/walkingtour/data/Post.java
@@ -1,8 +1,19 @@
 package uk.ac.aber.group12.walkingtour.data;
 
+import java.net.MalformedURLException;
+import java.net.URL;
+
 /**
  * Created by lot15 on 28/01/2014.
  */
 public class Post {
-
+    public static boolean postJSON(String urlString) {
+        try {
+            URL url = new URL(urlString);
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+            return false;
+        }
+        return true;
+    }
 }
