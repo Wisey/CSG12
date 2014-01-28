5f81ff7aaa460b9233d7ee9b51ab1fb4bab2c348
Louis Taylor <kragniz@gmail.com>
2014-01-28 11:46:18 +0000

    Open the connection

--- SRC/Drafts/WalkingTour/src/uk/ac/aber/group12/walkingtour/data/Post.java
+++ SRC/Drafts/WalkingTour/src/uk/ac/aber/group12/walkingtour/data/Post.java
@@ -1,7 +1,9 @@
 package uk.ac.aber.group12.walkingtour.data;
 
+import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.net.URLConnection;
 
 /**
  * Created by lot15 on 28/01/2014.
@@ -10,7 +12,11 @@ public class Post {
     public static boolean postJSON(String urlString) {
         try {
             URL url = new URL(urlString);
+            URLConnection conn = url.openConnection();
         } catch (MalformedURLException e) {
+            e.printStackTrace();
+            return false;
+        } catch (IOException e) {
             e.printStackTrace();
             return false;
         }
