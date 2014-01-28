9c7569dde3f89d8fd826ece909917836a55d039a
Louis Taylor <kragniz@gmail.com>
2014-01-28 13:57:10 +0000

    Add key checking to mock POST accepter

--- SRC/Drafts/Mocks/receive.py
+++ SRC/Drafts/Mocks/receive.py
@@ -17,6 +17,7 @@ def shutdown_server():
 @app.route('/', methods=['GET', 'POST'])
 def hello_world():
     if request.method == 'POST':
+        print request.data.strip()
         body = json.loads(request.data.strip())
 
         fields = (body[name] for name in (
@@ -26,16 +27,21 @@ def hello_world():
                       )
                   )
 
-        if len(body['locations']) > 0 and type(body['name']) is unicode:
-            os.system('figlet IT WORKS')
-            if not TEST:
-                shutdown_server()
+        if body.has_key('locations') and body.has_key('name'):
+            if len(body['locations']) > 0 and type(body['name']) is unicode:
+                os.system('figlet IT WORKS')
+                if not TEST:
+                    shutdown_server()
+            else:
+                os.system('figlet MALFORMED')
+
         else:
             os.system('figlet BADNESS')
 
         return 'success!'
         
     else:
+        os.system('figlet GETed')
         return 'POST to this url, please\n'
 
 if __name__ == '__main__':
