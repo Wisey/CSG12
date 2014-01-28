c1602c91bbf0b5be0a3cd2432bc14ef506b8be83
Louis Taylor <kragniz@gmail.com>
2014-01-28 10:03:01 +0000

    Add data fields to Location

--- SRC/Drafts/WalkingTour/src/uk/ac/aber/group12/walkingtour/data/Location.java
+++ SRC/Drafts/WalkingTour/src/uk/ac/aber/group12/walkingtour/data/Location.java
@@ -1,5 +1,9 @@
 package uk.ac.aber.group12.walkingtour.data;
 
 public class Location {
-
+    private String name;
+    private String image;
+    private double latitude;
+    private double longitude;
+    private double time;
 }
