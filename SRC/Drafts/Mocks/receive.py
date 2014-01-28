beb99068c54148c5b7e9a27f6b271529bc6d8dd5
Louis Taylor <kragniz@gmail.com>
2014-01-28 09:42:30 +0000

    Add verification code to receive.py

--- SRC/Drafts/Mocks/receive.py
+++ SRC/Drafts/Mocks/receive.py
@@ -1,11 +1,40 @@
 from flask import Flask, request
 import json
+import sys
+import os
+
 app = Flask(__name__)
+
+TEST = True
+strings_types = (unicode, str)
+
+def shutdown_server():
+    func = request.environ.get('werkzeug.server.shutdown')
+    if func is None:
+        raise RuntimeError('Not running with the Werkzeug Server')
+    func()
 
 @app.route('/', methods=['GET', 'POST'])
 def hello_world():
     if request.method == 'POST':
-        print json.loads(request.data.strip())
+        body = json.loads(request.data.strip())
+
+        fields = (body[name] for name in (
+                      'name',
+                      'short-description',
+                      'long-description'
+                      )
+                  )
+        print [f for f in fields]
+        field_types = (type(f) in strings_types for f in fields)
+        print field_types
+
+        if all_str and len(body['locations']) > 0:
+            os.system('figlet IT WORKS')
+            if not TEST:
+                shutdown_server()
+
+        return 'success!'
         
     else:
         return 'POST to this url, please\n'
