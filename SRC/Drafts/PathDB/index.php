9aad7b65234dc8dbdd8219c346828acf4dfc4db8
Wisey <BountyTom@gmail.com>
2014-01-30 16:11:59 +0000

    Update index.php
    Photo banner

--- SRC/Drafts/PathDB/index.php
+++ SRC/Drafts/PathDB/index.php
@@ -158,12 +158,12 @@ while($line = mysql_fetch_array($result, MYSQL_ASSOC)) {
 	
 <?php
 include('config.php');
-$result = mysql_query("SELECT * FROM photos1");
+$result = mysql_query("SELECT * FROM photos");
 while($row = mysql_fetch_array($result))
 {
-	
+	$data = $row['photoName'];
   	 echo '<div class="single"><div class="wrap">
-  		  <a href="'.$row['location'].'" rel="lightbox[plants]" title="'.$row['caption'].'"><img src="'.$row['location'].'" alt="Plants: image 1 0f 4 thumb" /></a>
+  		  <a href="'.$row['location'].'" rel="lightbox[plants]" title="'.$row['caption'].'"><img src="data:image/jpg;base64,'. $data . '/>"' alt="Plants: image 1 0f 4 thumb" /></a>
   		</div></div>';
 }				
 ?>		
