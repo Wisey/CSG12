a9ad33657ec785ee3e53b554ef87a8ca827aac16
benjimoola <bea16@aber.ac.uk>
2014-01-30 16:17:57 +0000

    Update index.php

--- SRC/Drafts/PathDB/index.php
+++ SRC/Drafts/PathDB/index.php
@@ -159,7 +159,7 @@ while($line = mysql_fetch_array($result, MYSQL_ASSOC)) {
 <?php
 include('config.php');
 $result = mysql_query("SELECT * FROM photos");
-while($row = mysql_fetch_array($result))
+while($photograph = mysql_fetch_array($getphotos))
 {
 	$data = $row['photoName'];
   	 echo 
