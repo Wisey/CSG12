b99533d7bc21c851e07a765aecb0c198ac9f187b
Wisey <BountyTom@gmail.com>
2014-01-30 10:11:02 +0000

    Update upload.php
    It's ALIIIIIIIVE

--- SRC/Drafts/PathDB/upload.php
+++ SRC/Drafts/PathDB/upload.php
@@ -15,7 +15,7 @@ echo("JSON Supported.</br>");
 
 //-------------------------------------------------------------------------------
 //create a DB connection
-$con = mysql_connect("localhost","root","group12");
+$con = mysql_connect("localhost","root","");
 if (!$con)
 {
 	die('Could not connect: ' . mysql_error());
@@ -39,12 +39,35 @@ file_put_contents($file, $current);
 $jsonDecode=json_decode(file_get_contents("post_data.json"));
 print_r($jsonDecode);
 echo("</br>");
-$result=array();
+$pathdata=array();
+$x = 0;
  
 foreach($jsonDecode as $key => $value)
 {
 	echo "<p>$key | $value</p>";
+	$pathdata[$x]=$value;
+	$x++;
 }
+mysql_query("INSERT INTO walks (title, shortDesc, longDesc, hours, distance) VALUES ('$pathdata[0]', '$pathdata[1]', '$pathdata[2]', '$pathdata[3]', '$pathdata[4]')");
+
+echo "ARRAY ACCESS";
+
+$pointMarkers = json_decode(file_get_contents("post_data.json"));
+$y=0;
+foreach($pointMarkers->locations as $mypoints)
+{
+	foreach($mypoints as $key => $value)
+	{
+		echo "<p>$key | $value</p>";
+		$pointdata[$y]=$value;
+		$y++;
+	}
+	mysql_query("INSERT INTO location (latitude, longitude) VALUES ('$pointdata[0]', '$pointdata[1]')");
+	mysql_query("INSERT INTO placedesc (name, description) VALUES ('$pointdata[2]', '$pointdata[3]')");
+	$y = 0;
+}
+
+mysql_close($con);
 
 ?>
 
