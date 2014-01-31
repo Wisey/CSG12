755d227d52968766690f4fc6b9d246b6e34edc9e
Wisey <BountyTom@gmail.com>
2014-01-31 09:35:41 +0000

    Update index.php

--- SRC/Drafts/PathDB/index.php
+++ SRC/Drafts/PathDB/index.php
@@ -86,10 +86,9 @@ mysql_select_db("pathdb", $con);
 			$photograph = mysql_fetch_array($getphotos);
 			$data = $photograph['photoName'];
 		?>
-			//'<img src="data:image/jpg;base64,<?=$data?>" />';
 			var LatLng = new google.maps.LatLng(<?=$a['latitude']?>,<?=$a['longitude']?>);
 			var ContentString = "<b><?=$description['name']?></b></br><?=$description['description']?>";
-			ContentString = <?=echo '<img src="data:image/jpg;base64,' . $data . '" />'?>;
+			//ContentString =
 			var marker = new google.maps.Marker(
 			{
 				map:map,
