0d1d0486a43ea057458353c0eebbd04c10601d78
Wisey <BountyTom@gmail.com>
2014-01-31 11:51:01 +0000

    Update index.php
    Reversion

--- SRC/Drafts/PathDB/index.php
+++ SRC/Drafts/PathDB/index.php
@@ -80,7 +80,8 @@ mysql_select_db("pathdb", $con);
 			$data = $photograph['photoName'];
 		?>
 			var LatLng = new google.maps.LatLng(<?=$a['latitude']?>,<?=$a['longitude']?>);
-			var ContentString = "<b><?=$description['name']?></b></br> <?=$description['description']?>;
+			var ContentString = "<b><?=$description['name']?></b></br> <?=$description['description']?></br> <img src='icon.2.png";
+			//ContentString =
 			var marker = new google.maps.Marker(
 			{
 				map:map,
@@ -145,6 +146,13 @@ mysql_select_db("pathdb", $con);
 
 <?php
 
+
+$getphotos = mysql_query ("SELECT * FROM photos");
+while($photograph = mysql_fetch_array($getphotos))
+{
+	$data = $photograph['photoName'];
+	echo '<img src="data:image/jpg;base64,' . $data . '" />     ';
+}
 ?>
 
 </body>
