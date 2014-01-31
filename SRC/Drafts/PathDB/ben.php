4db4093aa46e85192cb729024f2385cfd33f3be0
benjimoola <bea16@aber.ac.uk>
2014-01-31 11:20:33 +0000

    Update ben.php

--- SRC/Drafts/PathDB/ben.php
+++ SRC/Drafts/PathDB/ben.php
@@ -5,10 +5,7 @@ if (!$con)
 {
 	die('Could not connect: ' . mysql_error());
 }
-else
-{
-	echo('Connection Established.');
-}
+
 //pathdb STORES ALL TABLES.
 mysql_select_db("pathdb", $con);
 ?>
@@ -34,7 +31,7 @@ mysql_select_db("pathdb", $con);
 <?php
 	$options = mysql_query("SELECT * FROM walks");
 ?>
-<form action="index.php" method="post" style="height:25px; float:left;">
+<form action="index.php" method="post" style="height:15px; float:left;">
 <select name="select1"  style="width:134px; float:left; margin-left:10px; margin-top:10px;">
 <?php
 	while($line = mysql_fetch_array($options))
@@ -45,12 +42,8 @@ mysql_select_db("pathdb", $con);
 	}
 ?>
 </select>
-<input name = "submitbutton" type = "submit" value = "go" style=" margin-top:10px; height:25px; width:50px; background-color:grey; color:black;" />
+<input name = "submitbutton" type = "submit" value = "submit" />
 </form>
-<ul>
-	<li class="active"><a href="#">Home</a></li>
-	<li><a href="#">About</a></li>
-</ul>
 </nav>
 		
 <script type="text/javascript" src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCgXd8GzR2kAhJw-fnQqX_ZYpDnBxLLiRw&sensor=false">
@@ -81,9 +74,14 @@ mysql_select_db("pathdb", $con);
 			$dataselector = $a['ID'];
 			$data = mysql_query("SELECT * FROM placedesc WHERE locationID = '$dataselector'");
 			$description = mysql_fetch_array($data);
+			
+			$getphotos = mysql_query ("SELECT * FROM photos WHERE placeID = '$dataselector'");
+			$photograph = mysql_fetch_array($getphotos);
+			$data = $photograph['photoName'];
 		?>
 			var LatLng = new google.maps.LatLng(<?=$a['latitude']?>,<?=$a['longitude']?>);
-			var ContentString = "<b><?=$description['name']?></b></br><?=$description['description']?>";
+			var ContentString = "<b><?=$description['name']?></b></br> <?=$description['description']?></br> <img src='icon.2.png";
+			//ContentString =
 			var marker = new google.maps.Marker(
 			{
 				map:map,
@@ -100,47 +98,6 @@ mysql_select_db("pathdb", $con);
 		<?php
 		}
 		?>
-		
-		/*
-		userroute = [
-		new google.maps.LatLng(52.415100,-4.063118),
-		new google.maps.LatLng(52.415779,-4.062887),
-		new google.maps.LatLng(52.408504,-4.059840),
-		new google.maps.LatLng(52.408371,-4.059551),
-		new google.maps.LatLng(52.404819,-4.064931),
-		new google.maps.LatLng(52.399982,-4.072388),
-		new google.maps.LatLng(52.408867,-4.074300),
-		new google.maps.LatLng(52.409698,-4.085050),
-		new google.maps.LatLng(52.411865,-4.085286),
-		new google.maps.LatLng(52.412354,-4.088138),
-		new google.maps.LatLng(52.413265,-4.086802),
-		new google.maps.LatLng(52.413319,-4.085775),
-		new google.maps.LatLng(52.413448,-4.085581),
-		new google.maps.LatLng(52.412949,-4.083897),
-		new google.maps.LatLng(52.414127,-4.082470),
-		new google.maps.LatLng(52.414204,-4.082309),
-		new google.maps.LatLng(52.414177,-4.081690),
-		new google.maps.LatLng(52.414452,-4.081775),
-		new google.maps.LatLng(52.415977,-4.077967),
-		new google.maps.LatLng(52.416061,-4.079947),
-		new google.maps.LatLng(52.416687,-4.081175),
-		new google.maps.LatLng(52.420563,-4.084423),
-		new google.maps.LatLng(52.416473,-4.085048),
-		new google.maps.LatLng(52.417122,-4.083452),
-		new google.maps.LatLng(52.415943,-4.081427),
-		new google.maps.LatLng(52.416092,-4.083629),
-		new google.maps.LatLng(52.416119,-4.083943),
-		new google.maps.LatLng(52.415936,-4.084753),
-		new google.maps.LatLng(52.415176,-4.085528),
-		new google.maps.LatLng(52.414875,-4.086166),
-		new google.maps.LatLng(52.413898,-4.086434),
-		new google.maps.LatLng(52.413960,-4.086737),
-		new google.maps.LatLng(52.415321,-4.086872),
-		new google.maps.LatLng(52.415630,-4.087333),
-		new google.maps.LatLng(52.415661,-4.087775),
-		new google.maps.LatLng(52.424648,-4.082924)
-		];
-		*/
 		
 		var userroute = new Array();
 		<?php
@@ -186,5 +143,17 @@ mysql_select_db("pathdb", $con);
 <script src="js/jquery-ui-1.8.18.custom.min.js"></script>
 <script src="js/jquery.smooth-scroll.min.js"></script>
 <script src="js/lightbox.js"></script>
+
+<?php
+
+
+$getphotos = mysql_query ("SELECT * FROM photos");
+while($photograph = mysql_fetch_assoc($getphotos))
+{
+	$data = $photograph['photoName'];
+	echo '<img src="data:image/jpg;base64,' . $data . '" />';
+}
+?>
+
 </body>
 </html>
